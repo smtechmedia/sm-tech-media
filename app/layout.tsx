1f27e91b7634801221beb01156a6bb61e5801777
Sunil Marapatla <smtechmedia206@gmail.com>
2026-07-28 14:32:45 +0530

    Added organization schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -137,6 +137,46 @@ export default function RootLayout({
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+        <Script
+  id="organization-schema"
+  type="application/ld+json"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "@id": "https://www.smtechmedia.com/#organization",
+
+      name: "SM Tech Media",
+
+      url: "https://www.smtechmedia.com",
+
+      logo: "https://www.smtechmedia.com/logo.png",
+
+      image: "https://www.smtechmedia.com/logo.png",
+
+      description:
+        "SM Tech Media is a professional digital marketing agency providing website development, graphic designing, video editing, social media management, YouTube SEO and political campaign services.",
+
+      email: "smtechmedia206@gmail.com",
+
+      telephone: "+919908540952",
+
+      foundingDate: "2025",
+
+      founder: {
+        "@type": "Person",
+        name: "Sunil Marapatla",
+      },
+
+      sameAs: [
+        "https://www.facebook.com/SMTechMediaOfficial",
+        "https://www.instagram.com/sm_tech_media",
+        "https://www.youtube.com/@SMMovieEvents"
+      ],
+    }),
+  }}
+/>
       </head>
 
       <body className="flex min-h-full flex-col">{children}</body>
